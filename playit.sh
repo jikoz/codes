d66a44cdf1d2935a024f3498968c388bd5ce9362
jikoz <rajshekhar5894@gmail.com>
2026-03-05 16:18:13 +0530

    Create playit.sh

---
+++ playit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Watermark JikozYT
+echo "JikozYT Playit Installer"
+sleep 2
+
+# Banner
+cat << "EOF"
+██╗██╗██╗  ██╗ ██████╗ ███████╗██╗   ██╗████████╗
+     ██║██║██║ ██╔╝██╔═══██╗╚══███╔╝╚██╗ ██╔╝╚══██╔══╝
+     ██║██║█████╔╝ ██║   ██║  ███╔╝  ╚████╔╝    ██║   
+██   ██║██║██╔═██╗ ██║   ██║ ███╔╝    ╚██╔╝     ██║   
+╚█████╔╝██║██║  ██╗╚██████╔╝███████╗   ██║      ██║   
+ ╚════╝ ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝      ╚═╝
+EOF
+echo "╔══════════════════╗"
+echo "║CODE MADE BY JIKOZYT ║"
+echo "╚══════════════════╝"
+sleep 2
+# One Click Playit Installer
+echo "Starting Playit Installation Process"
+sleep 2
+# Playit Code
+wget https://github.com/playit-cloud/playit-agent/releases/download/v0.15.26/playit-linux-amd64
+chmod +x playit-linux-amd64
+./playit-linux-amd64
